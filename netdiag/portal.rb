c5d237c942cf4b5bf64153c53ff2c61037c1ecda
David BARBION <david.barbion@ext.leroymerlin.fr>
2017-03-08 23:40:02 +0100

    use webkit2-gtk gem

--- netdiag/portal.rb
+++ netdiag/portal.rb
@@ -1,50 +1,47 @@
-require 'webkit-gtk'
+require 'webkit2-gtk'
 
 module Netdiag
   class WindowPortal < Gtk::Window
     def initialize(uri)
       super
       @uri = uri
-      @view = WebKitGtk::WebView.new
-      @view.load_uri(@uri)
+      @view = WebKit2Gtk::WebView.new
       self.add(@view)
       self.set_default_size 544, 280
       self.window_position = :center
     end
 
     def reload_portal
-    puts "reload of #{@view.uri} asked"
+      @view.load_uri(@uri)
       @view.reload_bypass_cache
-    end
-
-    def hide
-      super
-      puts "hide"
     end
 
   end
 
   class Portal
     def initialize(uri="http://httpbin.org")
+      @keep_open=false
       @uri = uri
       @opened = false
+    end
+
+    def open_portal_authenticator_window(args={})
+      return if self.is_opened?
       @window = Netdiag::WindowPortal.new(@uri)
       @window.signal_connect("delete-event") do
+        @keep_open=false
         self.close_portal_authenticator_window
         true
       end
-    end
-
-    def open_portal_authenticator_window
-      return if self.is_opened?
+      @keep_open=true if args[:keep_open]
       @window.reload_portal
       @window.show_all
       @opened = true
     end
 
     def close_portal_authenticator_window
-      return if self.is_closed?
-      @window.hide
+      return if self.is_closed? or @keep_open
+      @window.destroy
       @opened = false
     end
 
