9da8741c94b7e3c4e0cb1809afbfdd50fb391ed5
David BARBION <david.barbion@ext.leroymerlin.fr>
2017-03-08 23:40:35 +0100

    add a menu for opening the captive portal window

--- netindic.rb
+++ netindic.rb
@@ -36,6 +36,7 @@ class Netindic
   end
 
   def initialize
+    @portal_authenticator = Portal.new
     @config = Netdiag::Config.new()
     @ai = AppIndicator::AppIndicator.new("Netdiag", "indicator-messages", AppIndicator::Category::COMMUNICATIONS);
     @indicator_menu = Gtk::Menu.new
@@ -44,7 +45,14 @@ class Netindic
       self.show_diag
     end
     @indicator_diagnose.show
+    @indicator_menu = Gtk::Menu.new
+    @indicator_captive = Gtk::MenuItem.new :label => "Open captive portal authenticator window"
+    @indicator_captive.signal_connect "activate" do
+      @portal_authenticator.open_portal_authenticator_window(:keep_open => true)
+    end
+    @indicator_captive.show
     @indicator_menu.append @indicator_diagnose
+    @indicator_menu.append @indicator_captive
     @indicator_exit     = Gtk::MenuItem.new :label => "Exit"
     @indicator_exit.signal_connect "activate" do
       Gtk.main_quit
@@ -64,7 +72,6 @@ class Netindic
     @dns = Netdiag::DNS.new
     @internet = Netdiag::Internet.new(@config.get_test_url)
 
-    @portal_authenticator = Portal.new
   end
 
   def prepare_diag
@@ -184,7 +191,7 @@ class Netindic
       end
     when STATE_EINTERNET
       if @last_state != STATE_EINTERNET
-        Libnotify.show(:summary => "Internet unreachable", :body => "Can't go outside local network, check filtering, gateways or cable/ADSL modem", :timeout => 2.5)
+        Libnotify.show(:summary => "Internet unreachable", :body => "Can't go outside local network, check filtering, gateways or cable/ADSL modem\n#{@internet.error}", :timeout => 2.5)
         @last_state = STATE_EINTERNET
         puts "error 4"
         @ai.set_icon("error_64")
