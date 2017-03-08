9678974d3d04c1a0e78ad50ef261b69bab3358af
David BARBION <david.barbion@ext.leroymerlin.fr>
2017-03-08 10:02:39 +0100

    handle opening and closing of captive portal window

--- netdiag/portal.rb
+++ netdiag/portal.rb
@@ -1,25 +1,50 @@
 require 'webkit-gtk'
 
 module Netdiag
-  class Portal < Gtk::Window
-  
-    def initialize(uri="http://httpbin.org")
+  class WindowPortal < Gtk::Window
+    def initialize(uri)
       super
       @uri = uri
-      signal_connect("destroy") do
-        self.close_portal_authenticator_window
-      end
-      @opened = false
       @view = WebKitGtk::WebView.new
-      @view.load_uri(uri)
+      @view.load_uri(@uri)
       self.add(@view)
+      self.set_default_size 544, 280
+      self.window_position = :center
+    end
+
+    def reload_portal
+    puts "reload of #{@view.uri} asked"
+      @view.reload_bypass_cache
+    end
+
+    def hide
+      super
+      puts "hide"
+    end
+
+  end
+
+  class Portal
+    def initialize(uri="http://httpbin.org")
+      @uri = uri
+      @opened = false
+      @window = Netdiag::WindowPortal.new(@uri)
+      @window.signal_connect("delete-event") do
+        self.close_portal_authenticator_window
+        true
+      end
     end
 
     def open_portal_authenticator_window
-      self.show_all
+      return if self.is_opened?
+      @window.reload_portal
+      @window.show_all
       @opened = true
     end
+
     def close_portal_authenticator_window
+      return if self.is_closed?
+      @window.hide
       @opened = false
     end
 
