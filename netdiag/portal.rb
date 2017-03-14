0f31d1e4eb7dff5285349c3684b6e5e23c3881fe
David BARBION <david.barbion@ext.leroymerlin.fr>
2017-03-14 17:51:10 +0100

    add support for page reloading when an error is throwed, typically a connection reset by peer

--- netdiag/portal.rb
+++ netdiag/portal.rb
@@ -1,26 +1,55 @@
 require 'webkit2-gtk'
 
+RELOAD_MAX_TRIES=50 # this seems a little bit overkill...
+
 module Netdiag
 
   class WindowPortal < Gtk::Window
-    def initialize(uri)
+    def initialize(url)
       super
-      @uri = uri
+      @url = url
+      @redirect_url = ''
+      @last_url = ''
       @view_context = WebKit2Gtk::WebContext.new
-      #@view_context.set_tls_errors_policy(0)
       @view_context.set_tls_errors_policy(WebKit2Gtk::TLSErrorsPolicy::IGNORE)
       @view = WebKit2Gtk::WebView.new(@view_context)
       self.add(@view)
       self.set_default_size 544, 380
       self.window_position = :center
+
+      # stores URI when loading state changed
+      # this permits to keep the redirection URL (ie, the captive portal URL)
+      @view.signal_connect("load-changed") do |web_view, load_event, user_data|
+        @redirect_url = web_view.uri if load_event == WebKit2Gtk::LoadEvent::REDIRECTED
+        @last_url = web_view.uri if load_event == WebKit2Gtk::LoadEvent::FINISHED
+      end
+
+      # intercept load errors, this can be caused by network problem,
+      # bad response from captive portal
+      # if such an error is catched, netdiag will try to reload the portal page
+      # for RELOAD_MAX_TRIES
+      @view.signal_connect("load-failed") do |web_view, load_event, failing_uri, error|
+#        puts "load failed, #{@tries} tries left"
+#        puts "URI is #{failing_uri}"
+        self.retry_load_portal
+      end
+    end
+   
+    # reload the current page
+    def retry_load_portal
+      return false if @tries == 0
+      @tries -= 1
+      @view.load_uri(@url)
     end
 
+    # initiate a new load
     def reload_portal
-      @view.load_uri(@uri)
-      @view.reload_bypass_cache
+      @tries = RELOAD_MAX_TRIES
+      @view.load_uri(@url)
+#      @view.reload_bypass_cache
     end
 
-    def uri
+    def url
       @view.uri
     end
 
@@ -47,7 +76,7 @@ module Netdiag
       @window = Netdiag::WindowPortal.new(args[:uri])
       @window.signal_connect("delete-event") do
         @keep_open=false
-        uri = @window.uri
+        uri = @window.url
         self.close_portal_authenticator_window
         Thread.new do
           self.signal_emit("portal_closed", uri)
