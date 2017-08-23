5ed5980f8308851c264eee2f00f230a4f19cdf26
David Barbion <davidb@230ruedubac.fr>
2017-08-23 10:29:08 +0200

    improve remote host problem detection

--- netdiag/internet.rb
+++ netdiag/internet.rb
@@ -27,8 +27,18 @@ module Netdiag
       rtt = 0.0
       (1..@count).each do
         res=self.get_uri
-        if res[:result].is_a?(Net::HTTPSuccess)
+        case res[:result]
+        when Net::HTTPSuccess
           quality += 100
+          count += 1
+          @error = ""
+        when STATE_INTERNET_EEXPIRED
+          @error = "timeout"
+        when STATE_INTERNET_EUNKNOWN
+          @error = res[:error]
+        when Net::HTTPServiceUnavailable
+          @error = "service unavailable"
+          quality += 75
           count += 1
         end
         rtt += res[:rtt]
@@ -48,7 +58,8 @@ module Netdiag
         res = self.get_uri
         # internet can't be reached or test server unavailable
         # in this case, a captive portal cannot be detected, let's give a chance
-        return false if res[:result] == STATE_INTERNET_EEXPIRED 
+        return false if res[:result] == STATE_INTERNET_EEXPIRED or 
+                        res[:result].is_a?(Net::HTTPServiceUnavailable)
 
         # usually, captive portal is done by
         # 1) sending a 302 to the client, redirecting to the portal, only works when client connect to http (not https)
@@ -81,17 +92,15 @@ module Netdiag
         res = http.request_get(uri.path)
         ret[:result] = res
       rescue Net::OpenTimeout => e # Connection timeout
-        @error = e.message
         puts "get_uri(): Net::OpenTimeout #{e.message}"
         ret[:result] = STATE_INTERNET_EEXPIRED
       rescue Net::ReadTimeout => e # Connection ok, but too long to respond
-        @error = e.message
         puts "get_uri(): Net::ReadTimeout #{e.message}"
         ret[:result] = STATE_INTERNET_EEXPIRED
       rescue Exception => e
-        @error = e.message
         puts "get_uri(): #{e.message}"
         ret[:result] = STATE_INTERNET_EUNKNOWN
+        ret[:error] = e.message
       end
       ret[:rtt] = Time.now - start
       return ret
